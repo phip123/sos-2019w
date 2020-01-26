b3dcc6114216dba8b46c09a7f470782d4ebd003d
Axel <axel.brunnbauer@tuwien.ac.at>
2020-01-26 20:41:47 +0100

    switched to plotly

--- somvisualization/visualizations/topographic_error.py
+++ somvisualization/visualizations/topographic_error.py
@@ -1,14 +1,7 @@
 from typing import Tuple, Dict
-import numpy as np
-from bokeh.io import show
-from bokeh.plotting import figure
-
-from somvisualization.io.somlib import parse
-from typing import Tuple, Dict
 
 import numpy as np
-from bokeh.io import show
-from bokeh.plotting import figure
+import plotly.express as px
 
 from somvisualization.io.somlib import parse
 
@@ -17,8 +10,7 @@ def visualize(map_info: Dict, winner_mapping: Dict):
     x_dim = int(map_info['XDIM'])
     y_dim = int(map_info['YDIM'])
     units = np.zeros((x_dim, y_dim), dtype=int)
-    x = []
-    y = []
+
     for winner in winner_mapping['MAPPING'].values():
         bmu = winner[0]
         sbmu = winner[1]
@@ -26,17 +18,10 @@ def visualize(map_info: Dict, winner_mapping: Dict):
         sbmu_pos = (sbmu[0], sbmu[1])
 
         if dist(bmu_pos, sbmu_pos) > 1:
-            x.append(bmu_pos[0])
-            y.append(bmu_pos[1])
             units[bmu_pos[0], bmu_pos[1]] += 1
 
-    H, xe, ye = np.histogram2d(x, y, bins=100)
-
-    # produce an image of the 2d histogram
-    p = figure(x_range=(0, x_dim), y_range=(0, y_dim), title='Image', match_aspect=True)
-    p.image(image=[units], x=0, y=0, dw=x_dim, dh=y_dim, palette="Spectral11")
-
-    show(p)
+    fig = px.imshow(units)
+    fig.show()
 
 
 def dist(a: Tuple[int, int], b: Tuple[int, int]) -> int:
@@ -44,6 +29,6 @@ def dist(a: Tuple[int, int], b: Tuple[int, int]) -> int:
 
 
 if __name__ == '__main__':
-    map_info = parse('10Clusters-large.map')
-    winner_mapping = parse('10Clusters-large.dwm')
+    map_info = parse('10Clusters-small.map')
+    winner_mapping = parse('10Clusters-small.dwm')
     visualize(map_info=map_info, winner_mapping=winner_mapping)
